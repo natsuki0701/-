75478e59fe91d642cbecbd8a08e5e7e7a582b7cb
natsuki0701 <76605574+natsuki0701@users.noreply.github.com>
2021-02-24 15:00:50 +0900

    1章 チェックテスト
    1章 チェックテスト.java

--- Check/Check.java
+++ Check/Check.java
@@ -19,16 +19,16 @@ public class Check{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i <5; i++) {
-            for (int j = i; j >0; j--) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = data.length-1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[i-1] > data[i]){
-                  int box = data[i];
-                  data[i] = data[i-1];
-                  data[i-1] = box;
+                if(data[i] > data[j]){
+                  int box = data[j];
+                  data[j] = data[i];
+                  data[i] = box;
                 }
             }
         }
