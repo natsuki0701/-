ae43ff21db745cd39472ec9eb3cd087b8a8cf1f0
natsuki0701 <76605574+natsuki0701@users.noreply.github.com>
2021-08-17 18:24:20 +0900

    条件分岐・演算子
    課題

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,28 @@
+console.log(10 === 10);
+
+let b = 12;
+if( b >= 10 && b <= 20 ){
+}
+else if (b % 2 === 0) {
+    console.log(b);
+}
+
+
+let x = 10;
+if (x >= 10 && x <= 20) {
+    console.log('成功です');
+}
+else {
+    console.log('失敗です'); 
+}
+
+
+let y = 0;
+//偶数の条件式
+if (y % 2 === 0) {
+    console.log('偶数です');
+}
+//奇数の条件式
+if (y % 2 !== 0) {
+    console.log('奇数です');
+}
